468e700fa4f3cc943b169b8a4ea4a087aeb6e11c
Manish Gupta <manish.gupta@nordcloud.com>
2022-09-19 11:46:41 +0300

    First Commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    # Greet the Person
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+
+name = input("Your Name: ")
+
+print(greet(name))
